d303a41fe118f25e7af27d4ba6024f54496fffbe
michael <michaelbutterfield1996@gmail.com>
2020-08-02 16:04:14 -0500

    Done with strategy and simulation test

--- pytrade/simulation/simulation.py
+++ pytrade/simulation/simulation.py
@@ -1,9 +1,10 @@
-from datetime import date, datetime
+import datetime
 from ..strategy import Strategy
 from ..portfolio import Portfolio
+import pandas as pd
 
 class Simulation:
-    def __init__(self, context, buy_power, strategy, start_date, name="", end_date = date.today()):
+    def __init__(self, context, buy_power, strategy, start_date, name="", end_date = datetime.date.today()):
         """Simulation initialization
 
         Args:
@@ -11,24 +12,30 @@ class Simulation:
             start_date ([type]): [description]
             end_date ([type], optional): [description]. Defaults to date.today().
             strategy (Strategy): Strategy to run the simulation.
+            context: (Dataframe): Full data set for simulation
         """
         self.name = name
         self.buy_power = buy_power
         self.strategy = strategy
         self.start_date = start_date
         self.end_date = end_date
+        self.context = context
         # Enable passing in portfolio as initial starting point
-        self._portfolio = Portfolio(name, context)
+        self._portfolio = Portfolio(name, None)
     
     def run(self):
         # Initialize Portfolio
-        self.initialize(self.buy_power)
-        
+        self.initialize()
+
         current_date = self.start_date
 
         # Run Simulation
         while(current_date <= self.end_date):
             print(current_date)
+            # Update portfolio context with new date's data
+            current_date_mask = current_date >= self.context.index.date
+            self._portfolio.context = pd.DataFrame(self.context[current_date_mask])
+
             # Apply strategy actions / transactions
 
                 #Check to see if stocks need to be sold
@@ -38,11 +45,29 @@ class Simulation:
                 # Update buy power (dividends/new deposit)
                 
                 # Buy new stocks
-            current_date += datetime.timedelta(days=1)
+            if len(self.context.index) <= (self.context.index.get_loc(current_date) + 1):
+                break
+            current_date = self.context.index[self.context.index.get_loc(current_date) + 1]
 
+        print("Finished Simulation")
+        print(self._portfolio.report())
         return self._portfolio
         # Do something with results.
     
-    def initialize(self, buy_power):
+    def initialize(self):
+        self._portfolio.deposit(self.buy_power)
+
+        start_date_mask = self.start_date >= self.context.index.date
+        self._portfolio.context = pd.DataFrame(self.context[start_date_mask])
+
         # Buy stocks and put into Portfolio in accordance with strategy
-        pass
+        to_buy = self.strategy.initialize(self._portfolio)
+        for stock in to_buy:
+            name = stock[0]
+            percentage = stock[1]
+            price = self._portfolio.context["Close"][name].iloc[-1]
+            num_shares = (self.buy_power / price) * percentage
+            self._portfolio.buy(name, num_shares, self.buy_power * percentage)
+
+        print("Finished Initialization")
+        print(self._portfolio.report())
--- pytrade/strategy/strategy.py
+++ pytrade/strategy/strategy.py
@@ -1,6 +1,12 @@
+from ..portfolio import Portfolio
+import pandas as pd
+
 class Strategy:
     def __init__(self, name):
         self.name = name
+        pass
+
+    def initialize(self, portfolio):
         pass
 
     def to_buy(self, portfolio):
@@ -17,17 +23,24 @@ class TSLA_Strategy(Strategy):
     def __init__(self, name):
         Strategy.__init__(self, name)
 
+    def initialize(self, portfolio):
+        return [("TSLA", 0.5)]
+    
     def to_buy(self, portfolio):
         # Look at result of analysis to determine what to buy
-        return ["TSLA"]
+        if "TSLA" in portfolio.stocks and portfolio.context["Close"]["TSLA"].iloc[-1] < 1500:
+            return []
+        return [("TSLA", 1)]
 
     def to_sell(self, portfolio):
         # Look at result of analysis to determine what to sell
+        if "TSLA" in portfolio.stocks and portfolio.context["Close"]["TSLA"].iloc[-1] > 1500:
+            return [("TSLA", 1)]
         return []
 
     def analysis(self, portfolio):
         # Put data manipulation here that build triggers/metrics.
-        pass
+        self.results = pd.DataFrame()
     
 class Utkarsh_v1(Strategy):
     pass
